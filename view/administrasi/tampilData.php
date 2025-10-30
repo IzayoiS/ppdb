d45f6ab978e65bd60029400ca015069891117d47
IzayoiS <iqbalmhasby54@gmail.com>
2025-10-30 19:45:59 +0700

    feat: refactor tampilData.php for improved readability and data handling

--- view/administrasi/tampilData.php
+++ view/administrasi/tampilData.php
@@ -1,12 +1,9 @@
 <!-- Header -->
 <?php
-	$title = "Administrasi"; // Judulnya
-	require("../template/header.php"); // include headernya
+$title = "Administrasi";
+require("../template/header.php");
+include('../../config/connection.php');
 ?>
-
-
-
-<!-- Isinya -->
 
 <section class="section">
   <div class="section-header">
@@ -14,10 +11,10 @@
   </div>
 
   <?php
-  	if (isset($_SESSION['alert'])) {
-  		echo $_SESSION['alert'];
-  		unset($_SESSION['alert']);
-  	}
+  if (isset($_SESSION['alert'])) {
+    echo $_SESSION['alert'];
+    unset($_SESSION['alert']);
+  }
   ?>
 
   <div class="section-body">
@@ -25,55 +22,75 @@
       <div class="col-12">
         <div class="card">
           <div class="card-header">
-            <!-- <h4>Basic DataTables</h4> -->
-            <a href="tambahData.php" type="button" class="btn btn-primary daterange-btn icon-left btn-icon">
-              <i class="fas fa-plus"></i> Tambah Data Administrasi 
-            </a>
+            <h4>Data Administrasi Siswa</h4>
           </div>
           <div class="card-body">
-
-            <!-- tabelnya -->
-            <div class="table-responsive" >
+            <div class="table-responsive">
               <table class="table table-striped" id="table-1">
                 <thead>
                   <tr>
-                    <th class="text-center"> # </th>
+                    <th class="text-center">#</th>
                     <th>NISN</th>
                     <th>Nama</th>
                     <th>Harga</th>
                     <th>Status Pembayaran</th>
                     <th>Tanggal Ubah</th>
-                    <th class="text-center">Action</th>
+                    <th class="text-center">Aksi</th>
                   </tr>
                 </thead>
                 <tbody>
                   <?php
-                  	include('../../config/connection.php');
+                  $no = 1;
+                  $data = mysqli_query($conn, "
+                    SELECT 
+                      b.Id_Identitas_Siswa,
+                      b.NISN,
+                      b.Nama_Peserta_Didik,
+                      a.id_administrasi,
+                      a.harga,
+                      a.status,
+                      a.tgl_ubah
+                    FROM identitas_siswa b
+                    LEFT JOIN administrasi a 
+                    ON b.Id_Identitas_Siswa = a.id_identitas_siswa
+                    ORDER BY b.Id_Identitas_Siswa ASC
+                  ") or die(mysqli_error($conn));
 
-                    $no = 1;
-                    $data = mysqli_query($conn, "SELECT a.*, b.NISN, b.Nama_Peserta_Didik FROM administrasi a 
-                                                      LEFT JOIN identitas_siswa b 
-                                                      ON a.id_identitas_siswa = b.Id_Identitas_Siswa") or die(mysqli_error($conn));
-                    foreach ($data as $row) { 
-                  ?>
-                  <tr>
-                  	<td><?= $no++; ?></td>
-                  	<td><?= $row['NISN']; ?></td>
-                  	<td><?= $row['Nama_Peserta_Didik']; ?></td>
-                  	<td><?= $row['harga']; ?></td>
-                  	<td><?= $row['status']; ?></td>
-                  	<td><?= $row['tgl_ubah']; ?></td>              	
-                    <td class="text-center" width="120px">
-                      <!-- <a href="#" class="btn btn-success my-2" data-toggle="modal" data-target="#exampleModal"><i class="fas fa-eye"></i></a> -->
-                      <a href="ubahData.php?id=<?= $row['id_administrasi']; ?>" class="btn btn-warning"><i class="fas fa-pencil-alt"></i></a>
-                      <a href="../../controller/admin/administrasi.php?hapusData=<?= $row['id_administrasi']; ?>" class="btn btn-danger my-2" onclick="return confirm('Anda Yakin');"><i class="fas fa-trash"></i></a>
-                  	</td>
-                  </tr>
-              	  <?php } ?>
+                  foreach ($data as $row) {
+                    ?>
+                    <tr>
+                      <td><?= $no++; ?></td>
+                      <td><?= $row['NISN']; ?></td>
+                      <td><?= $row['Nama_Peserta_Didik']; ?></td>
+                      <td><?= $row['harga'] ? number_format($row['harga']) : '-'; ?></td>
+                      <td>
+                        <?php if ($row['status'] == 'Lunas'): ?>
+                          <span class="badge badge-success">Lunas</span>
+                        <?php else: ?>
+                          <span class="badge badge-danger">Belum Lunas</span>
+                        <?php endif; ?>
+                      </td>
+                      <td><?= $row['tgl_ubah'] ?? '-'; ?></td>
+                      <td class="text-center">
+                        <td class="text-center">
+                          <?php
+                          $isLunas = ($row['status'] == 'Lunas');
+                          $btnClass = $isLunas ? 'btn-danger' : 'btn-success';
+                          $btnText = $isLunas ? '<i class="fas fa-times"></i> Batalkan Lunas' : '<i class="fas fa-check"></i> Tandai Lunas';
+                          ?>
+                          <form method="POST" action="../../controller/admin/administrasi.php" style="display:inline;">
+                            <input type="hidden" name="toggleLunas" value="1">
+                            <input type="hidden" name="id_administrasi" value="<?= $row['id_administrasi']; ?>">
+                            <input type="hidden" name="id_identitas_siswa" value="<?= $row['Id_Identitas_Siswa']; ?>">
+                            <button type="submit" class="btn <?= $btnClass ?> btn-sm"><?= $btnText ?></button>
+                          </form>
+                        </td>
+                      </td>
+                    </tr>
+
+                  <?php } ?>
                 </tbody>
               </table>
-            <!-- penutup tabelnya -->
-
             </div>
           </div>
         </div>
@@ -82,15 +99,23 @@
   </div>
 </section>
 
-<script type="text/javascript">
-	$(document).ready(function(){
-		$('#table-1').DataTable();
-	});
+<script>
+  $(document).ready(function () {
+    $('#table-1').DataTable();
+
+    // Pass data ke modal
+    $('#modalLunas').on('show.bs.modal', function (event) {
+      var button = $(event.relatedTarget);
+      var id = button.data('id');
+      var nama = button.data('nama');
+      var identitas = button.data('identitas');
+      $('#id_administrasi').val(id);
+      $('#nama_siswa').val(nama);
+      $('#id_identitas_siswa').val(identitas);
+    });
+
+  });
 </script>
 
-<!-- Penutup Isinya -->
-
-
-
 <!-- Footer -->
-<?php require("../template/footer.php");?>
+<?php require("../template/footer.php"); ?>
